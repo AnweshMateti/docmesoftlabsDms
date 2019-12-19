8ec28209fca5d000f374dc9554ca18c24e8ad313
Tester <Tester@192.168.1.2>
2019-12-19 15:51:03 +0530

    this another one

---
+++ src/helo/heloclass.java
@@ -0,0 +1,13 @@
+package helo;
+
+public class heloclass {
+
+	public static void main(String[] args) {
+	 heloclass hc=new heloclass();
+	 hc.add();
+
+	}
+public void add() {
+	System.out.println("add");
+}
+}
